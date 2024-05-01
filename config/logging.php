fc7ddf443262105463eac90641bd291bc165c5d7
Ali Sheraz <imalisheraz@gmail.com>
2024-05-01 12:55:15 +0500

    📦 NEW: Add Config fro cloudwatch

--- config/logging.php
+++ config/logging.php
@@ -85,7 +85,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
         ],
 
@@ -117,6 +117,21 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
-    ],
 
+        // Cloudwatch Channel
+        'cloudwatch' => [
+            'name' => env('CLOUDWATCH_LOG_NAME', ''),
+            'region' => env('CLOUDWATCH_LOG_REGION', ''),
+            'credentials' => [
+                'key' => env('AWS_ACCESS_KEY_ID', ''),
+                'secret' => env('AWS_SECRET_ACCESS_KEY', '')
+            ],
+            'stream_name' => env('CLOUDWATCH_LOG_STREAM_NAME', 'laravel_app'),
+            'retention' => env('CLOUDWATCH_LOG_RETENTION_DAYS', 14),
+            'group_name' => env('CLOUDWATCH_LOG_GROUP_NAME', 'laravel_app'),
+            'version' => env('CLOUDWATCH_LOG_VERSION', 'latest'),
+            'formatter' => \Monolog\Formatter\JsonFormatter::class,
+            'disabled' => env('DISABLE_CLOUDWATCH_LOG', false),
+        ],
+    ],
 ];
